8962516fe7fb61bf3c733975f241d560f185febb
apogrondin <apogrondin@gmail.com>
2024-07-19 20:12:34 +0200

    final class

---
+++ story_time.js
@@ -0,0 +1,32 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let protagonistName = "Alice";
+let antagonistName = "The Red Queen";
+let setting = "Wonderland";
+let conflict = "Alice must defeat the Red Queen to save Wonderland.";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let timePeriod = "Victorian era";
+let ageOfProtagonist = "12 years old";
+let numberChallenges = "three";
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let itemsPossessed = ["magic potion", "golden key", "pocket watch"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let hasWon = false;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(`--- Story of ${protagonistName} in ${setting} ---`);
+console.log(`Time Period: ${timePeriod}`);
+console.log(`Protagonist: ${protagonistName}, Age: ${ageOfProtagonist}`);
+console.log(`Antagonist: ${antagonistName}`);
+console.log(`Conflict: ${conflict}`);
+console.log(`Items Possessed: ${itemsPossessed.join(', ')}`);
+console.log(`Number of Challenges: ${numberChallenges}`);
+console.log(`Outcome: ${hasWon ? `${protagonistName} has won!` : `${protagonistName} has lost.`}`);
+
